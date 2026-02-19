3d3e7658984262bec8b157bdee8961d985433655
ArpanAk <akarpan2005@gmail.com>
2026-02-19 17:29:21 +0530

    finished the controller code

--- src/main/java/com/example/watchDog/Controller/ServiceController.java
+++ src/main/java/com/example/watchDog/Controller/ServiceController.java
@@ -2,21 +2,23 @@ package com.example.watchDog.Controller;
 
 import com.example.watchDog.Entity.Websites;
 import com.example.watchDog.Services.WebsiteCheckService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.watchDog.Services.WebsitePinger;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api")
 public class ServiceController {
 
     private final WebsiteCheckService websiteCheckService;
+    private final WebsitePinger pinger;
 
-    public ServiceController(WebsiteCheckService websiteCheckService){
+    public ServiceController(WebsiteCheckService websiteCheckService, WebsitePinger pinger){
         this.websiteCheckService = websiteCheckService;
+        this.pinger = pinger;
     }
 
     @GetMapping("/reports")
@@ -30,13 +32,20 @@ public class ServiceController {
         return websiteCheckService.getAllReports();
     }
 
-    @GetMapping
-    public String singleSite(@RequestParam String url){
-        return websiteCheckService.pingSingleSite(url);
+    @GetMapping("/single")
+    public CompletableFuture<String> singleSite(@RequestParam String url){
+        return pinger.pingSingleSite(url);
     }
 
-    @GetMapping
+    @GetMapping("/multiple")
     public List<String> multipleSite(@RequestParam String urls){
         return websiteCheckService.pingMultipleSites(urls);
+    }
+
+    @PostMapping("/pingAndSave")
+    public CompletableFuture<String> pingAndSave(@RequestBody Map<String , String> payload){
+        String url = payload.get("url");
+
+        return websiteCheckService.pingAndSaveSite(url);
     }
 }
--- src/main/java/com/example/watchDog/Services/WebsiteCheckService.java
+++ src/main/java/com/example/watchDog/Services/WebsiteCheckService.java
@@ -14,51 +14,51 @@ import java.util.stream.Collectors;
 @Service
 public class WebsiteCheckService {
 
-        private final WebsitePinger pinger;
-        private final RestTemplate restTemplate;
-        private final Repo websiteRepo;
+    private final WebsitePinger pinger;
+    private final RestTemplate restTemplate;
+    private final Repo websiteRepo;
 
-        public WebsiteCheckService(WebsitePinger pinger, Repo websiteRepo, RestTemplate restTemplate) {
-            this.websiteRepo = websiteRepo;
-            this.restTemplate = restTemplate;
-            this.pinger = pinger;
-        }
+    public WebsiteCheckService(WebsitePinger pinger, Repo websiteRepo, RestTemplate restTemplate) {
+        this.websiteRepo = websiteRepo;
+        this.restTemplate = restTemplate;
+        this.pinger = pinger;
+    }
 
-        public void checkAllWebsites(){
-            List<Websites> activeSites = websiteRepo.findActive();
+    public void checkAllWebsites() {
+        List<Websites> activeSites = websiteRepo.findActive();
 
-            for(Websites site : activeSites){
-                pinger.pingSingleSite(site.getUrl()).thenAccept(result -> {
-                    int statusCode = result.contains("up") ? 200 : (result.contians("TIMEOUT") ? 504 : 500);
+        for (Websites site : activeSites) {
+            pinger.pingSingleSite(site.getUrl()).thenAccept(result -> {
+                int statusCode = result.contains("UP") ? 200 : (result.contains("DOWN (TIMEOUT)") ? 504 : 500);
 
-                    websiteRepo.healthCheckLog(site.getId(), statusCode, 0);
-                    System.out.println("automated check completed for  :" + site.getUrl());
-                });
-            }
-        }
-
-        public List<Websites> getAllReports(){
-            return websiteRepo.detailedHealthLog();
-        }
-
-
-
-        public List<String> pingMultipleSites(String combinedUrl){
-            return Arrays.stream(combinedUrl.split("\\+"))
-                    .filter(url -> !url.isBlank())
-                    .map(String::trim)
-                    .map(pinger::pingSingleSite) // Start all pings in parallel
-                    .map(CompletableFuture::join) // Wait for them to finish (max 10s total)
-                    .collect(Collectors.toList());
-        }
-
-        public String pingAndSaveSite(String url){
-            Websites site = websiteRepo.findOrCreate(url);
-
-            return pinger.pingSingleUrl(url).thenApply(result -> {
-                // 3. Extract status and save (handles auto-delete of old logs)
-                int statusCode = result.contains("UP") ? 200 : 500;
                 websiteRepo.healthCheckLog(site.getId(), statusCode, 0);
-                return result;
+                System.out.println("automated check completed for  :" + site.getUrl());
             });
+        }
+    }
+
+    public List<Websites> getAllReports() {
+        return websiteRepo.detailedHealthLog();
+    }
+
+
+    public List<String> pingMultipleSites(String combinedUrl) {
+        return Arrays.stream(combinedUrl.split("\\+"))
+                .filter(url -> !url.isBlank())
+                .map(String::trim)
+                .map(pinger::pingSingleSite) // Start all pings in parallel
+                .map(CompletableFuture::join) // Wait for them to finish (max 10s total)
+                .collect(Collectors.toList());
+    }
+
+    public CompletableFuture<String> pingAndSaveSite(String url) {
+        Websites site = websiteRepo.findOrCreate(url);
+
+        return pinger.pingSingleSite(url).thenApply(result -> {
+            // 3. Extract status and save (handles auto-delete of old logs)
+            int statusCode = result.contains("UP") ? 200 : 500;
+            websiteRepo.healthCheckLog(site.getId(), statusCode, 0);
+            return result;
+        });
+    }
 }
